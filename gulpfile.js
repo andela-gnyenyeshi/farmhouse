6f9db0efdd21c34424271a9e0a623163879d9505
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-04-11 16:00:01 +0300

    [Ticket #11] Folder structure

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+(function(){
+  'use strict';
+
+  var gulp = require('gulp'),
+  jade = require('gulp-jade'),
+  less = require('gulp-less'),
+  path = require('path');
+
+  var paths = {
+    public: 'public',
+    images: 'app/images/**',
+    scripts: 'app/scripts/**/*.+(js|jsx)',
+    less: 'app/style/*.+(less|css)'
+  };
+})();
